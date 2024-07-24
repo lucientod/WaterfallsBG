e0747b041213d2d41e5a784b28808569ac9bc031
Lucien Todorov <lucientod@gmail.com>
2024-07-24 22:38:21 +0300

    add login and register pages

--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -1,5 +1,24 @@
-export default function Register( ){
-    return(
-        <h1>Register</h1>
+import styles from "./Register.module.css";
+
+export default function Login() {
+    return (
+        <form action="#" method="post" className={styles.login}>
+            <fieldset>
+                <legend>Register</legend>
+
+                <p className={styles.field}>
+                    <input type="email" name="email" id="email" required/>
+                        <label for="email">Email</label>
+                </p>
+
+                <p className={styles.field} >
+                    <input type="password" name="password" id="password" required/>
+                        <label for="password">Password</label>
+                </p>
+
+                <input type="submit" value="Register"/>
+
+            </fieldset>
+        </form>
     )
 }
