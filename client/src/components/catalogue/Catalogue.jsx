7065f787de3a691caf60f14897d92f72cd368d7b
Lucien Todorov <lucientod@gmail.com>
2024-07-25 22:18:16 +0300

    testVodopad in catalogue

--- client/src/components/catalogue/Catalogue.jsx
+++ client/src/components/catalogue/Catalogue.jsx
@@ -2,22 +2,37 @@ import styles from './Catalogue.module.css'
 
 import { Link } from 'react-router-dom'
 
+import { useFetch } from '../../hooks/useFetch.js';
 export default function Catalogue() {
+    const {
+        data: waterfalls,
+        isFetching,
+        refetch,
+    } = useFetch('http://localhost:3030/jsonstore/waterfalls', []);
+    console.log((Object.values(waterfalls)[1]));
+    const testVodopad = (Object.values(waterfalls)[1]);
+    console.log(testVodopad);
+
+    if (!testVodopad)
+        refetch()
+
     return (
         <div className={styles.wrapper}>
-            <article className={styles.wcCard}>
+
+            {!isFetching ? console.log("IS FETCHING"):
+           ( <article className={styles.wcCard}>
                 <div>
 
-                    <img src="./images/krushunskiVodopadi.jpg" />
+                    <img src={testVodopad.imageUrl} />
 
                 </div>
                 <div>
-                    <h3>Small info</h3>
-                    <p>Location</p>
-                    <p>Description Description Description Description Description Description Description Description Description Description </p>
+                    <h3>{testVodopad.info}</h3>
+                    {/* <p>Location</p> */}
+                    <p>{testVodopad.description}</p>
                     <Link to="/">Details</Link>
                 </div>
-            </article>
+            </article>)}
 
             <article className={styles.wcCard}>
                 <div>
