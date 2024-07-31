36c6eeb1e30ccbd1adcc39ba01a596d254ee9ac6
Lucien Todorov <lucientod@gmail.com>
2024-07-31 15:14:29 +0300

    catalogue css

--- client/src/components/catalogue/Catalogue.jsx
+++ client/src/components/catalogue/Catalogue.jsx
@@ -16,50 +16,57 @@ export default function Catalogue() {
 
 
     return (
+
         <div className={styles.wrapper}>
 
-            {isFetching ? console.log("IS FETCHING") :
-                (<article className={styles.wcCard}>
-                    <div>
+            {isFetching ? console.log("IS FETCHING")
+                :
+                <>
+                    <article className={styles.wcCard}>
+                        <div>
 
-                        <img src={testVodopad.imageUrl} />
+                            <img src={testVodopad.imageUrl} />
 
-                    </div>
-                    <div>
-                        <h3>{testVodopad.info}</h3>
-                        {/* <p>Location</p> */}
-                        <p>{testVodopad.description}</p>
-                        <Link to="/">Details</Link>
-                    </div>
-                </article>)}
+                        </div>
+                        <div className={styles.info}>
+                            <h3>{testVodopad.name}</h3>
+                            <p>{testVodopad.info}</p>
+                            <p>{testVodopad.description}</p>
+                            <Link to="/">Details</Link>
+                        </div>
+                    </article>
 
-            <article className={styles.wcCard}>
-                <div>
+                    <article className={styles.wcCard}>
+                        <div>
 
-                    <img src="./images/krushunskiVodopadi.jpg" />
+                            <img src="https://scontent.fsof5-1.fna.fbcdn.net/v/t1.18169-9/19894742_1506610096026707_4199415961955623974_n.jpg?stp=dst-jpg_p480x480&_nc_cat=107&ccb=1-7&_nc_sid=13d280&_nc_ohc=AJdxxZPRNhYQ7kNvgEwjbcV&_nc_ht=scontent.fsof5-1.fna&oh=00_AYCoSH91k-rNmFDgrjMiS4PduMNrPWf6U6qgOgDs1mZeFg&oe=66CB6F6A" />
 
-                </div>
-                <div>
-                    <h3>Small info</h3>
-                    <p>Location</p>
-                    <p>Description Description Description Description Description Description Description Description Description Description </p>
-                    <Link to="/">Details</Link>
-                </div>
-            </article>
+                        </div>
+                        <div className={styles.info}>
+                            <h3>Small info</h3>
+                            <p>Location</p>
+                            <p>Description Description Description Description Description Description Description Description Description Description </p>
+                            <Link to="/">Details</Link>
+                        </div>
+                    </article>
 
-            <article className={styles.wcCard}>
-                <div>
+                    <article className={styles.wcCard}>
+                        <div>
 
-                    <img src="./images/krushunskiVodopadi.jpg" />
+                            <img src="./images/krushunskiVodopadi.jpg" />
 
-                </div>
-                <div>
-                    <h3>Small info</h3>
-                    <p>Location</p>
-                    <p>Description Description Description Description Description Description Description Description Description Description </p>
-                    <Link to="/">Details</Link>
-                </div>
-            </article>
+                        </div>
+                        <div className={styles.info}>
+                            <h3>Small info</h3>
+                            <p>Location</p>
+                            <p>Description </p>
+                            <Link to="/">Details</Link>
+                        </div>
+                    </article>
+                </>
+            }
+
         </div>
+
     )
 }
--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -5,27 +5,29 @@ export default function Home() {
 
     return (
         <>
-            <div className={styles.body}>
-                <h1>Lorem ipsum dolor sit amet.</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet debitis corporis aperiam laboriosam? Beatae eos at voluptates, velit, dolorum saepe culpa provident deleniti quaerat modi voluptatibus nemo accusantium ipsa magni?</p>
+            <div className="wrapper">
+                <div className={styles.body}>
+                    <h1>Lorem ipsum dolor sit amet.</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet debitis corporis aperiam laboriosam? Beatae eos at voluptates, velit, dolorum saepe culpa provident deleniti quaerat modi voluptatibus nemo accusantium ipsa magni?</p>
 
-                <div className={styles.whArticle}>
-                    <article className={styles.whCard}>
-                        <img src="./images/krushunskiVodopadi.jpg" />
-                        <h4>Info Small Info Small Info Small Info Small Info Small 1</h4>
-                        <Link to="/">Details</Link>
-                    </article>
-                    <article className={styles.whCard}>
-                        <img src="./images/krushunskiVodopadi.jpg" />
-                        <h4>Info Small Info Small Info Small Info Small Info Small 2</h4>
-                        <Link to="/">Details</Link>
-                    </article> 
-                    <article className={styles.whCard}>
-                        <img src="./images/krushunskiVodopadi.jpg" />
-                        <h4>Info Small Info Small Info Small Info Small Info Small 3</h4>
-                        <Link to="/">Details</Link>
-                    </article>
-                   
+                    <div className={styles.whArticle}>
+                        <article className={styles.whCard}>
+                            <img src="./images/krushunskiVodopadi.jpg" />
+                            <h4>Info Small Info Small Info Small Info Small Info Small 1</h4>
+                            <Link to="/">Details</Link>
+                        </article>
+                        <article className={styles.whCard}>
+                            <img src="./images/krushunskiVodopadi.jpg" />
+                            <h4>Info Small Info Small Info Small Info Small Info Small 2</h4>
+                            <Link to="/">Details</Link>
+                        </article>
+                        <article className={styles.whCard}>
+                            <img src="./images/krushunskiVodopadi.jpg" />
+                            <h4>Info Small Info Small Info Small Info Small Info Small 3</h4>
+                            <Link to="/">Details</Link>
+                        </article>
+
+                    </div>
                 </div>
             </div>
         </>
